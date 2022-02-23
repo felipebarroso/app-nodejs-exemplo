e04b9391ee66e668c0df974444def93c113dd939
Felipe Barroso <felipe.queiroz.barroso@gmail.com>
2022-02-23 10:34:41 -0300

    alteracao projeto exemplo puc

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const path = require('path')
+const app = express()
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.get('/', (req, res) => {
+  res.send('Felipe Barroso')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
